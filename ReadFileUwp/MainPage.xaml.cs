806409f2cfa08cd3d085fc5dde9b56fdda9cc9f4
Micke Wessén <micke.wessen@hotmail.com>
2020-10-14 16:00:43 +0200

    Add project files.

---
+++ ReadFileUwp/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using SharedLibraries;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ReadFileUwp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+
+        private async void btnJson_Click(object sender, RoutedEventArgs e)
+        {
+
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+            picker.FileTypeFilter.Add(".json");
+            picker.FileTypeFilter.Add(".txt");
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+
+            string text = await Windows.Storage.FileIO.ReadTextAsync(file);
+
+            List<Person> DeserializedProducts = JsonConvert.DeserializeObject<List<Person>>(text);
+
+            ListView.ItemsSource = DeserializedProducts;
+
+        }
+
+        private void btnCsv_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void btnXml_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
